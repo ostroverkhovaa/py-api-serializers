481d530aff6b255e4a181f1878c83bff23c11983
Anastasiia <skubakovaa@gmail.com>
2025-08-12 14:15:41 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,48 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Genre, Actor, Movie, CinemaHall, MovieSession
+from cinema.serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    MovieListSerializer,
+    MovieRetriesSerializer,
+    MovieSerializer,
+    CinemaHallSerializer, MovieSessionListSerializer, MovieSessionSerializer, MovieSessionRetriesSerializer
+)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+        elif self.action == "retrieve":
+            return MovieRetriesSerializer
+        return MovieSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieSessionListSerializer
+        elif self.action == "retrieve":
+            return MovieSessionRetriesSerializer
+        return MovieSessionSerializer
